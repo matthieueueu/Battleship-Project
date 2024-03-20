d70e121a70cfe21a624f4a7c6ac1b0e0796a11a9
Matt MIC <matthieumichelet99@gmail.com>
2024-03-20 11:36:50 +0200

    new changes 20.03.24  11:36

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ const battleshipArray =[
     //4
     [0,0,0,0,0,0,0,0],
     //5
-    [0,0,0,0,0,0,0,0],
+    [0,0,0,1,1,1,0,0],
     //6
     [0,0,0,0,0,0,0,0],
     //7
@@ -25,6 +25,16 @@ const battleshipArray =[
 ]
 counter = 0;
 
+// create event listener on table
+// this will watch all events in the table
+// when you click anywhere on the table
+alert("YOU HAVE FIVE GUESSES. EVERY WRONG GUESS GOES ON THE COUNTER. BUY ME A COFFEE.")
+
+
+function sayHi() {
+    console.log('yo! event happened')
+}
+
 function checkHit(x,y) {
 
     // check matrix[x][y] has 1 or 0
@@ -32,13 +42,22 @@ function checkHit(x,y) {
 
     
     console.log('hi')
+    console.log()
     
 
     if(battleshipArray[x][y] == 1){
         print("ship hit!")
+        const idName = "matrix" + y + x
+        console.log('idName:', idName)
+        document.getElementById(idName).innerHTML = `X`
+
+        //how to put X on clicked box?? document.something().innerHTML = `` ig but how to reference form inside function??
+        document.getElementById("empty").innerHTML = `Counter = ${counter}`
+        // how to get element reference by clicking in HTML and using it in JS
     }
     else{
         counter+=1
+        document.getElementById("empty").innerText = `Counter = ${counter}`
         print("missed")  
     }
     if(counter>=5){
